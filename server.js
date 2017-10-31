86826dc655d360117f5ad89756663f272820ccf4
Brennen Murphy <brennenamurphy@gmail.com>
2017-10-31 16:18:40 -0600

    all pages work

---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+
+var app = express();
+
+
+
+app.use(express.static("./public"));
+
+app.get('/', function(request, response){
+  response.sendFile('./index.html', {root: './'});
+  console.log("sent index");
+});
+
+
+
+
+app.get('/greeley', function(request, response){
+    response.sendFile('./public/html/greeley.html', {root: './'});
+    console.log("sent greeley");
+});
+
+app.get('/fort-collins', function(request, response){
+    response.sendFile('./public/html/fort-collins.html', {root: './'});
+    console.log("sent fort");
+});
+
+app.get('/denver', function(request, response){
+    response.sendFile('./public/html/denver.html', {root: './'});
+    console.log("sent denver");
+});
+
+app.get('/breckenridge', function(request, response){
+    response.sendFile('./public/html/breckenridge.html', {root: './'});
+    console.log("sent breck");
+});
+
+app.get('/glenwood', function(request, response){
+    response.sendFile('./public/html/glenwood.html', {root: './'});
+    console.log("sent glenwood");
+});
+
+app.listen(8080, function(){
+  console.log('running on port 8080')
+})
