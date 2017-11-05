79a8196e208cf3d2b02210518ad6ed9ea3311afe
Brennen Murphy <brennenamurphy@gmail.com>
2017-11-04 18:03:14 -0600

    cargo functions added

--- public/js/cargo.js
+++ public/js/cargo.js
@@ -1,3 +1,4 @@
+Vue.config.devtools = true;
 var newVM = new Vue ({
   el: "#app",
   data: {
@@ -9,23 +10,45 @@ var newVM = new Vue ({
     cash: 200,
   },
   methods: {
+    increaseCargo: function(item, weight, cash) {
+      newVM[item] ++;
+      newVM.weight -= weight;
+      newVM.cash -= cash;
+      console.log(newVM[item]);
+      if (newVM.cash < 0) {
+        if (newVM.weight < 0){
+          newVM.decreaseCargo(item,weight,cash);
+          alert("Not enough cash and too much weight");
+        }
+        else {
+        newVM.decreaseCargo(item,weight,cash);
+        alert("Not enough Cash");
+        }
+      }
+      else if (newVM.weight < 0){
+        newVM.decreaseCargo(item,weight,cash);
+        alert("Too much weight");
+
+      }
+
+    },
+    decreaseCargo: function(item, weight, cash) {
+      newVM[item] --;
+      newVM.weight += weight;
+      newVM.cash += cash;
+      console.log(newVM[item]);
+      if (newVM[item] < 0) {
+        newVM.increaseCargo(item,weight,cash);
+        alert("You don't have that many");
+
+      }
+    },
+    checkClass: function(weight, cash) {
+      console.log(cash)
+      console.log(newVM.cash)
+      }
+
 
   }
 
-})
-
-function increaseCargo(item, weight, cash) {
-  newVM[item] ++;
-  newVM.weight -= weight;
-  newVM.cash -= cash
-  console.log(newVM["item"])
-  console.log(newVM.beer)
-}
-
-function decreaseCargo(item, weight, cash) {
-  newVM[item] --;
-  newVM.weight += weight;
-  newVM.cash += cash;
-
-  console.log(newVM.beer)
-}
+});
